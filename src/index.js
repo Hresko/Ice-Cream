762e2a9915495baed9cc025cfe5d74a413749c5a
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-06-26 17:36:24 +0300

    about-modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+// Modal window
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[about-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
 
 // ========= header - scroll =========
 window.onscroll = () => changeHeaderBackground();
