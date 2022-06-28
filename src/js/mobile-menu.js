51f8ac8d2b80bb4533825ee041596f3f3da0ddb8
JuliaGolban <julia.j.shcherban@gmail.com>
2022-06-28 08:29:21 +0300

    split js

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,26 @@
+// ========= mobile menu =========
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+
+// When clicks anywhere outside of the modal, close it
+
+// refs.modal.forEach(item => {
+//   item.addEventListener('click', e => {
+//     let currentMenu = e.currentTarget;
+//     const targetMenu = e.target;
+//     closeModal(currentMenu);
+//     closeAndStopVideo(targetMenu);
+//   });
+// });
