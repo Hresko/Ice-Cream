5446d13fee3f9031cba56e8c8f2b48f0d3d7969e
Hresko <86959462+Hresko@users.noreply.github.com>
2022-06-28 12:13:47 +0000

    Deploying to gh-pages from @ Hresko/Ice-Cream@177d40ec0ca76c817ba6f304f75152d110f98fe3 🚀

---
+++ index.e2734e30.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-menu-open]"),closeModalBtn:document.querySelector("[data-modal-menu-close]"),modal:document.querySelector("[data-modal-menu]"),body:document.querySelector("#body")};function o(){document.body.classList.toggle("modal-menu-is-open"),e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.e2734e30.js.map
