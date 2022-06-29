6cb253525d66b1b2e8a99360ab342345cd12c513
JuliaGolban <julia.j.shcherban@gmail.com>
2022-06-29 16:37:31 +0300

    change js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,25 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
 
-  menuBtnRef.addEventListener('click', () => {
+  const toggleBurgerMenu = () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
     mobileMenuRef.classList.toggle('is-open');
-  });
+    !expanded ? fixedModalOverflow() : closeModalOverflow();
+    !expanded && setActiveLink(document.querySelectorAll('.navigation-link'));
+  };
+
+  menuBtnRef.addEventListener('click', toggleBurgerMenu);
 })();
+
+// Fixed window when modal opened
+
+function fixedModalOverflow() {
+  document.querySelector('body').style.overflow = 'hidden';
+}
+
+function closeModalOverflow() {
+  document.querySelector('body').style.overflow = 'unset';
+}
